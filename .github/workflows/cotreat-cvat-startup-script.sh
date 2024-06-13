4a4bffc97f866969783b8a75e922cc309c6a8427
Neekey Ni <ni184775761@gmail.com>
2024-06-13 22:16:13 +1000

    add github action script

---
+++ .github/workflows/cotreat-cvat-startup-script.sh
@@ -0,0 +1,22 @@
+# Create a temporary directory
+TEMP_DIR=$(mktemp -d)
+
+# Load the zip file from Google Cloud Storage to the temporary directory
+gsutil cp gs://cotreat-cvat/deploy.zip $TEMP_DIR
+
+# Unzip the file to the temporary directory
+unzip $TEMP_DIR/deploy.zip -d $TEMP_DIR
+
+# Sync the unzipped folder with the existing /cvat folder
+rsync -av --delete $TEMP_DIR/deploy/ /cvat/
+
+# Set environment variables
+export CVAT_HOST=cvat.cotreat.io
+export ACME_EMAIL=engineering@cotreat.com.au
+
+# Rebuild the application in the /cvat folder
+cd /cvat
+sudo -E docker compose -f docker-compose.yml -f docker-compose.dev.yml -f docker-compose.https.yml up --build -d
+
+# Clean up the temporary directory
+rm -rf $TEMP_DIR
