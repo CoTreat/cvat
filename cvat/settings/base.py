dd1f59376c97b174f13aef4a7e1a57de9705525e
David Lin <david.lin@cotreat.com.au>
2025-11-18 09:02:23 +1100

    CA-303: job details page performance boost (#12)
    CA-302: update configuration for boosting job page performance

--- cvat/settings/base.py
+++ cvat/settings/base.py
@@ -570,7 +570,8 @@ redis_ondisk_port = os.getenv("CVAT_REDIS_ONDISK_PORT", 6666)
 redis_ondisk_password = os.getenv("CVAT_REDIS_ONDISK_PASSWORD", "")
 
 # Sets the timeout for the expiration of data chunk in redis_ondisk
-CVAT_CHUNK_CACHE_TTL = 3600 * 24  # 1 day
+# Set to7 days (default), configurable via env var. CVAT original default is 1 day.
+CVAT_CHUNK_CACHE_TTL = int(os.getenv("CVAT_CHUNK_CACHE_TTL", 3600 * 24 * 7))
 
 # Sets the timeout for the expiration of preview image in redis_ondisk
 CVAT_PREVIEW_CACHE_TTL = 3600 * 24 * 7  # 7 days
